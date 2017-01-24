b8f69e91f88eb62e7de9272348faa14b36e3a05a
Spencer Gerdes <skgerdes@yahoo.com>
2017-01-24 13:19:34 -0600

    Added variable nstring in function, function now returns both string and nstring, and test sentence was changed.

--- 001-disemvoweler.js
+++ 001-disemvoweler.js
@@ -1,6 +1,6 @@
 function vowelEater(string){
-	string = string.replace(/a/g, "").replace(/e/g, "").replace(/i/g, "").replace(/o/g, "").replace(/u/g, "").replace(/ /g, "")
-	return string;
+	var nstring = string.replace(/a/g, "").replace(/e/g, "").replace(/i/g, "").replace(/o/g, "").replace(/u/g, "").replace(/ /g, "")
+	return string + " => *nom* *nom* => " + nstring ;
 }
 
-console.log(vowelEater("Hello"))
+console.log(vowelEater("I enjoy a good use of the vowels."))
