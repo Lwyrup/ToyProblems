3744c338da7440fae4fde61f43e70d3362b6075c
Spencer Gerdes <skgerdes@yahoo.com>
2017-01-24 13:35:41 -0600

    Function logs numOfDie and faceOfDie

--- 002-diceroller.js
+++ 002-diceroller.js
@@ -3,7 +3,7 @@ function rollForInitative(string){
 	var inputNums = string.split("d");
 	var numOfDie = parseInt(inputNums[0]);
 	var faceOfDie = parseInt(inputNums[1]);
-
+	console.log("A " + faceOfDie + " sided die is rolled " + numOfDie + " times.")
 
 	return
 }
