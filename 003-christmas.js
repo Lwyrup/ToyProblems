a17694a221e7a50b5e9298795ae48f5899d5e6f8
Spencer Gerdes <skgerdes@yahoo.com>
2017-01-25 10:08:57 -0600

    Changed fundamental structure.

--- 003-christmas.js
+++ 003-christmas.js
@@ -1,13 +1,13 @@
 var gifts = [" Partridge in a Pear Tree", " Turtle Doves", " French Hens", " Calling Birds", " Golden Rings", 
 			 " Geese a Laying", " Swans a Swimming", " Maids a Milking", " Ladies Dancing", " Lords a Leaping", 
 			 " Pipers Piping", " Drummers Drumming"];
-var len = gifts.length;
+var arr = []
 
-for(var i = 12; i > 0; i--){
-		console.log("1" + gifts[1])
-		if(i == 11){continue}
-		console.log("and a" + gifts[0])
-		if(i == 12){continue}
+for(i = 0; i < gifts.length; i++){
+	console.log("On the first day of christmas\n my true love gave to me: ")
+	arr.push(gifts[i])
+	console.log(arr)
+	console.log("\n")
 }
 
 
