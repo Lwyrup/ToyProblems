f0f7f18ca645c3d00fa4ab996961dce646defc30
Spencer Gerdes <skgerdes@yahoo.com>
2017-01-25 09:30:29 -0600

    Created problem

---
+++ 003-christmas.js
@@ -0,0 +1,14 @@
+var gifts = [" Partridge in a Pear Tree", " Turtle Doves", " French Hens", " Calling Birds", " Golden Rings", 
+			 " Geese a Laying", " Swans a Swimming", " Maids a Milking", " Ladies Dancing", " Lords a Leaping", 
+			 " Pipers Piping", " Drummers Drumming"];
+var len = gifts.length;
+
+for(var i = 12; i > 0; i--){
+		console.log("1" + gifts[1])
+		if(i == 11){continue}
+		console.log("and a" + gifts[0])
+		if(i == 12){continue}
+}
+
+
+
