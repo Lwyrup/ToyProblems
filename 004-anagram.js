05291e494644d34692c8f24f31958f511ef6d0bb
Spencer Gerdes <skgerdes@yahoo.com>
2017-01-26 09:36:06 -0600

    Created and working.

---
+++ 004-anagram.js
@@ -0,0 +1,11 @@
+function anagramChecker(array){
+	var formated = []
+	var sorted = []
+	for(var i = 0; i < array.length; i++){
+		formated.push(array[i].split(" ").join("").toLowerCase())
+		sorted.push(formated[i].split("").sort());
+	}
+return console.log( "Is [" + array + "] anagram: " + (sorted[0].join("") === sorted[1].join("")));
+}
+
+anagramChecker(["Clint Eastwood", "Old West Action"]);
