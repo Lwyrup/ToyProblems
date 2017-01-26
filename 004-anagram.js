61bb3f2a78801df96c4b2c5361119b36f6592e2a
Spencer Gerdes <skgerdes@yahoo.com>
2017-01-26 09:49:44 -0600

    Changed

--- 004-anagram.js
+++ 004-anagram.js
@@ -2,7 +2,7 @@ function anagramChecker(array){
 	var formated = []
 	var sorted = []
 	for(var i = 0; i < array.length; i++){
-		formated.push(array[i].split(" ").join("").toLowerCase())
+		formated.push(array[i].replace(/ /g, "").toLowerCase())
 		sorted.push(formated[i].split("").sort());
 	}
 return console.log( "Is [" + array + "] anagram: " + (sorted[0].join("") === sorted[1].join("")));
