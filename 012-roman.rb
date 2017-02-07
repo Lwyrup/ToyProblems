6d220d403c22a048d61ad27bda545f61b0ad7433
Spencer Gerdes <skgerdes@yahoo.com>
2017-02-07 09:41:46 -0600

    Toying with a hash

--- 012-roman.rb
+++ 012-roman.rb
@@ -1,15 +1,7 @@
 require'pry'
 
 #converstion table and count
-values = [
-m = 1000,
-d = 500,
-c = 100,
-l = 50,	
-x = 10,
-v = 5,
-i = 1]
-
+values = { "m" => 1000, "d" => 500, "c" => 100, "l" => 50,	 "x" => 10, "v" => 5, "i" => 1}
 
 origin = 364
 input = origin
